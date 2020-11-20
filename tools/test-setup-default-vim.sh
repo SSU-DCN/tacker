78f2c1663d385c9d7a13b1f3d7e0767e7589dc33
Hiep Mai <maiquanghiepdl@gmail.com>
2020-11-20 06:02:46 +0000

    Change deployment's Api Version to apps/v1

--- tools/test-setup-default-vim.sh
+++ tools/test-setup-default-vim.sh
@@ -4,3 +4,9 @@
 # for functional testing, which cannot be put
 # in devstack/plugin.sh because new zuul3 CI
 # cannot keep the devstack plugins order
+
+openstack --os-username nfv_user --os-project-name nfv --os-password devstack --os-auth-url http://192.168.5.5/identity --os-project-domain-name Default --os-user-domain-name Default vim register --is-default --description "Default VIM" --config-file /opt/stack/data/tacker/vim_config.yaml VIM0
+
+
+openstack --os-username nfv_user --os-project-name nfv --os-password devstack --os-auth-url http://192.168.5.5/identity --os-project-domain-name Default --os-user-domain-name Default vim register --is-default --description "Default VIM" --config-file /opt/stack/data/tacker/vim_config.yaml VIM0
+
